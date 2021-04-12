f69ec1e50ff4ff7597ab388f8e95ee7501ec8f64
Tommy Munson <Tmunson50@MacBook-Air.local>
2021-04-12 12:40:55 -0500

    more updates

--- static/app.js
+++ static/app.js
@@ -1,54 +1,64 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
 // Build Table 
-function tableDisplay(ufoSightings) {
-    var tbody = d3.select("tbody");
-    ufoSightings.forEach((ufoRecord)) => {
+function importTable(table) {
+    table.forEach(function(file) {
         var row = tbody.append("tr");
-        Object.defineProperties(ufoRecord).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.html(value);
-        });
-    };
-};
+        Object.entries(file).forEach(function([key,value]) {
+            var cell = tbody.append("td")
+            cell.text(value);
+        })
+    })
+}
 
-// Clear Table For New Data
-function deleteTbody() {
-    d3.select("tbody")
-    .selectAll("tr").remove()
-    .selectAll("td").remove();
-};
+// Import the Data
+importTable(tableData);
 
-// Display All UFO Sightings
-console.log(tableData);
-tableDisplay(tableData);
-
-// Filter Button
 var button = d3.select("#filter-btn");
 
-// Filter DB & Display
-button.on("click",function(event) {
+var resetButton = d3.select("#reset-btn");
+
+var form = d3.select("#form-group");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+resetButton.on("click", reset);
+
+// filter table by date
+
+function runEnter() {
+
+    // Prevent Defaults
     d3.event.preventDefault();
-    deleteTbody();
-    var dateInput = d3.select("#datetime").property("value");
+    var inputElement = d3.select("#datetime")
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(inputDate => inputDate.datetime == inputValue);
 
-    if (dateInput.trim() === "" ) {
-        var filteredData = tableData;
-    } else {
-        var filteredData  = tableData.filter(ufoSighting =>
-            ufoSighting.datetime === dateInput.trim());
-    };
 
-    // If no data is found
-    if (filteredData.length == 0) {
-        d3.select("tbody")
-        .append("tr")
-        .append("td")
-            .attr("colspan",7)
-            .html("<h4>No Records Found</h4>");
-    };
-    console.log(filteredData);
-    tableDisplay(filteredData);
-})
+    // Clear Body
+    tbody.html("");
+
+    // Reload filtered data via html table
+    importTable(filteredData);
+
+}
+
+// Reset whole page
+
+function reset () {
+    // No defaults!
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    // Clear Body
+    tbody.html("");
+
+    // Reload filtered data via html table
+    importTable(tableData);
+}
+
 
